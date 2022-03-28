c5ff9a1dbe05bcb7917d1a210186727603634731
Norhen hgui <98626871+Hguinorhen@users.noreply.github.com>
2022-03-29 01:41:43 +0200

    Components

---
+++ src/Components/Profil/Address.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Address() {
+  return (
+    <div  >
+          <h1>Billing address: Tunisia_Sousse_Sahloul 
+              <br/>
+              Email: Haguinorhen@gmail.Com 
+              <br/>
+              Tel : 54 047 142 
+          </h1>
+    </div>
+  );
+}
+
+export default Address
---
+++ src/Components/Profil/FullName.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FullName() {
+    return <div >
+        <pre>          HELLO THERE ,
+               <br /> 
+               <br/> 
+            I'm  Norhn .  </pre> 
+        
+    </div>;
+}
+
+export default FullName 
